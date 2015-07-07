c1c414b917af0f102e3fe9ed2708ec2813e144d9
han4wluc <han4wluc@gmail.com>
2015-07-07 21:41:33 +0800

    fix start.sh

--- start.sh
+++ start.sh
@@ -2,15 +2,15 @@
 
 echo "Please enter your webisite name:"
 read website_name
-sed -i s/website_name/$website_name/g execute.sh
+sed -i s/website_name/$website_name/g setting.sh
 
 echo "Please enter your webisite url (without http://) :"
 read website_url
-sed -i s/website_url/$website_url/g execute.sh
+sed -i s/website_url/$website_url/g setting.sh
 sed -i s/website_url/$website_url/g config/config.js
 
 echo "Please enter your port number:"
 read website_port
-sed -i s/website_port/$website_port/g execute.sh
+sed -i s/website_port/$website_port/g setting.sh
 
 sh ./setting.sh
